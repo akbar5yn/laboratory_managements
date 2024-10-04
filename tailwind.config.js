eae7d6cfa72c5f6fd55ecf7ab82472f8a081103c
Akbar <akbar5yn.developer@gmail.com>
2024-10-04 13:56:28 +0700

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    // require('tailwindcss-filters')
+  ],
+}
+
