74d6b17dee9f9b4b07df7394889670dd430b3f74
Akbar <akbar5yn.developer@gmail.com>
2024-10-04 16:23:49 +0700

    create some route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard', ['title' => 'Beranda']);
+});
+
+Route::get('/inventaris', function () {
+    return view('inventaris', ['title' => 'Invetaris']);
 });
