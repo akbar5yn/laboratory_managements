c315bdce625b208bb141c38b8c00f5e283adfdd5
Akbar <akbar5yn.developer@gmail.com>
2024-10-06 23:55:54 +0700

    mengubah box shadow custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
       fontFamily: {
         poppins: ['Poppins'],
       },
+      boxShadow: {
+        'menu-inset': 'inset 0 -4px 6px rgba(0, 0, 0, 0.1)'
+      }
     },
   },
   plugins: [
