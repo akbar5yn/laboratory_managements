e501fc18a4f7d008c90fe94874f7800ae3a95a3a
Akbar <akbar5yn.developer@gmail.com>
2024-10-07 01:17:38 +0700

    melakukan penyesuaian style

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,4 @@
-<header class="header bg-[#f6fafd] py-5 px-7 flex justify-between items-center rounded-xl shadow-md">
-    <h1 class="font-medium text-2xl">{{ $slot }}</h1>
+<header class="header bg-[#f6fafd] py-5 px-4 flex justify-between items-center rounded-xl shadow-md">
+    <h1 class="font-medium text-xl">{{ $slot }}</h1>
     <h4>Hi, Akbar Pratama S</h4>
 </header>
