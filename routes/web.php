e1788c2cca691bba5ebca802ba34d6aa52b3241f
Akbar <akbar5yn.developer@gmail.com>
2024-10-11 20:06:58 +0700

    membuat beberapa data dummy

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -48,16 +49,205 @@ Route::get('/dashboard', function () {
 });
 
 Route::get('/inventaris-alat', function () {
-    return view('inventaris-alat', ['title' => 'Kelola Alat & Barang']);
+    return view('inventaris-alat', ['title' => 'Kelola Alat & Barang', 'tools' => [
+        [
+            'id' => '1',
+            'id_alat' => 'NEA',
+            'nama_alat' => 'Neraca 800 series',
+            'lokasi' => 'Lab Fisika Dasar',
+            'tahun_pengadaan' => '2016',
+            'fungsi_alat' => 'Menimbang sampel ',
+        ],
+        [
+            'id' => '2',
+            'id_alat' => 'KIT',
+            'nama_alat' => ' Kit Praktikum Elektronika Dasar ',
+            'lokasi' => 'Lab Elektronika dan Instrumentasi',
+            'tahun_pengadaan' => '2016',
+            'fungsi_alat' => 'Praktikum elektronika dasar ',
+        ],
+
+        [
+            'id' => '3',
+            'id_alat' => 'MUR',
+            'nama_alat' => 'Multimeter Digital',
+            'lokasi' => ' Lab Fisika Modern',
+            'tahun_pengadaan' => '2016',
+            'fungsi_alat' => ' Mengukur Arus dan Tegangan',
+        ],   
+        
+        [
+            'id' => '4',
+            'id_alat' => 'CAU',
+            'nama_alat' => 'Catu Daya',
+            'lokasi' => 'Lab Fisika Modern',
+            'tahun_pengadaan' => '2016',
+            'fungsi_alat' => 'Mengalirkan tegangan dengan mengubah tegangan AC ke DC ',
+        ],
+        [
+            'id' => '5',
+            'id_alat' => 'AUO',
+            'nama_alat' => ' Audio Frequency Generator GW/GRG-450B',
+            'lokasi' => 'Lab Elektronika dan Instrumentasi ',
+            'tahun_pengadaan' => '2016',
+            'fungsi_alat' => 'Untuk menghasilkan sinyal pada frekuensi tertentu ',
+        ],
+    ]]);
+});
+
+Route::get('/inventaris-alat/{id}', function ($id_alat){
+    $tools = [
+        [
+            [    
+                'id_alat' => 'NEA',
+                'no_unit' => 'NEA1',
+                'nama_alat' => 'Neraca 800 series',
+                'status' => 'Dipinjam',
+                'kondisi' => 'Normal',
+                
+            ],
+            [    
+                'id_alat' => 'NEA',
+                'no_unit' => 'NEA2',
+                'nama_alat' => 'Neraca 800 series',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ],
+            [    
+                'id_alat' => 'NEA',
+                'no_unit' => 'NEA3',
+                'nama_alat' => 'Neraca 800 series',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ],
+            [    
+                'id_alat' => 'NEA',
+                'no_unit' => 'NEA4',
+                'nama_alat' => 'Neraca 800 series',
+                'status' => 'Rusak',
+                'kondisi' => 'Rusak',
+                
+            ],
+        ],
+        [
+            [
+                'id_alat' => 'KIT',
+                'no_unit' => 'KIT1',
+                'nama_alat' => ' Kit Praktikum Elektronika Dasar ',
+                'status' => 'Dipinjam',
+                'kondisi' => 'Normal',
+                
+            ],
+            [
+                'id_alat' => 'KIT',
+                'no_unit' => 'KIT2',
+                'nama_alat' => ' Kit Praktikum Elektronika Dasar ',
+                'status' => 'Rusak',
+                'kondisi' => 'Rusak',
+                
+            ],
+            [
+                'id_alat' => 'KIT',
+                'no_unit' => 'KIT3',
+                'nama_alat' => ' Kit Praktikum Elektronika Dasar ',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ],
+        ],
+
+        [
+            [
+                'id_alat' => 'MUR',
+                'no_unit' => 'MUR1',
+                'nama_alat' => 'Multimeter Digital',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ],
+            [
+                'id_alat' => 'MUR',
+                'no_unit' => 'MUR2',
+                'nama_alat' => 'Multimeter Digital',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ],
+        ],   
+        
+        [
+            [
+                'id_alat' => 'CAU',
+                'no_unit' => 'CAU1',
+                'nama_alat' => 'Catu Daya',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ]
+        ],
+        [
+            [
+                'id_alat' => 'AUO',
+                'no_unit' => 'AUO1',
+                'nama_alat' => ' Audio Frequency Generator GW/GRG-450B',
+                'status' => 'Tersedia',
+                'kondisi' => 'Normal',
+                
+            ]
+        ],
+    ];
+
+    $flattenedTools = Arr::flatten($tools, 1);
+    $matchingTools = Arr::where($flattenedTools, function ($tool) use ($id_alat) {
+        return $tool['id_alat'] == $id_alat;
+    });
+    if (empty($matchingTools)) {
+        abort(404, 'Tool not found');
+    }
+    return view('detail-alat', [
+        'title' => 'Kelola Alat & Barang',
+        'subtitle' => 'Detail Alat',
+        'tools' => $matchingTools // Mengirimkan semua unit dengan id_alat yang sama
+    ]);
 });
 
 Route::get('/inventaris-ruangan', function () {
-    return view('inventaris-ruangan', ['title' => 'Kelola Ruangan']);
+    return view('inventaris-ruangan', ['title' => 'Kelola Ruangan', 'rooms' => [
+        [
+            'id' => '1',
+            'id_ruangan' => 'lab-01',
+            'nama_ruangan' => 'Lab Fisika Dasar',
+            'lokasi_ruangan' => 'Gedung Utama Lantai 1',
+            'kapasitas' => '30'
+        ],
+        [
+            'id' => '2',
+            'id_ruangan' => 'lab-02',
+            'nama_ruangan' => 'Lab Fisika Modern',
+            'lokasi_ruangan' => 'Gedung Utama Lantai 2',
+            'kapasitas' => '20'
+        ],
+        [
+            'id' => '3',
+            'id_ruangan' => 'lab-03',
+            'nama_ruangan' => 'Lab Elektronika dan Instrumentasi',
+            'lokasi_ruangan' => 'Gedung Utama Lantai 1',
+            'kapasitas' => '20'
+        ],
+        [
+            'id' => '4',
+            'id_ruangan' => 'lab-04',
+            'nama_ruangan' => 'Ruang Material 1 dan Material 5',
+            'lokasi_ruangan' => 'Gedung Utama Lantai 2',
+            'kapasitas' => '30'
+        ],
+    ]]);
 });
 
 Route::get('/jadwal-ruangan', function () {
-    return view('jadwal-ruangan', ['title' => 'Jadwal Ruangan',
-    'schedules' => [
+    return view('jadwal-ruangan', ['title' => 'Jadwal Ruangan', 'schedules' => [
         [
             'nim' => '2000016001',
             'name' => 'Teguh Dwi Cahya Kusuma',
@@ -94,8 +284,7 @@ Route::get('/jadwal-ruangan', function () {
             'log' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsam voluptatem itaque sint maiores voluptas delectus corporis vero quae mollitia!',
             'waktu' => '09:20 - 10:00'
         ],
-    ]
-]);
+    ]]);
 });
 
 Route::get('/peminjaman-alat', function () {
