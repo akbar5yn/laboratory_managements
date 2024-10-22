3bfe0108cea28f2334b26dd2f53e89ce2c6e232a
Akbar <akbar5yn.developer@gmail.com>
2024-10-23 00:49:38 +0700

    menambahkan import sweatesweetalert2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,14 @@
 import './bootstrap';
 import 'preline'
+import Swal from 'sweetalert2';
+
+function showAlert(title, text, icon) {
+    Swal.fire({
+        title: title,
+        text: text,
+        icon: icon,
+        confirmButtonText: 'OK',
+    });
+}
+
+window.showAlert = showAlert;
