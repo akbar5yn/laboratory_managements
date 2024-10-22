500ce2e2fe0490eb74feb5e13aea9c2c144d8822
Akbar <akbar5yn.developer@gmail.com>
2024-10-22 21:08:32 +0700

    menambahkan data jumlah pada seeder di table Equipment Category

--- database/seeders/EquipmentCategorySeeder.php
+++ database/seeders/EquipmentCategorySeeder.php
@@ -14,41 +14,46 @@ class EquipmentCategorySeeder extends Seeder
     {
         $equipmentCategories = [
             [
-                // 'slug' => 'NEA',
+                'slug' => 'NEA',
                 'nama_alat' => 'Neraca 800 series',
                 'lokasi' => 'Lab Fisika Dasar',
                 'tahun_pengadaan' => '2016',
                 'fungsi' => 'Menimbang sampel ',
+                'jumlah' => '4'
             ],
             [
-                // 'slug' => 'KIT',
-                'nama_alat' => ' Kit Praktikum Elektronika Dasar ',
+                'slug' => 'KIT',
+                'nama_alat' => 'Kit Praktikum Elektronika Dasar ',
                 'lokasi' => 'Lab Elektronika dan Instrumentasi',
                 'tahun_pengadaan' => '2016',
                 'fungsi' => 'Praktikum elektronika dasar ',
+                'jumlah' => '3'
             ],
 
             [
-                // 'slug' => 'MUR',
+                'slug' => 'MUR',
                 'nama_alat' => 'Multimeter Digital',
                 'lokasi' => 'Lab Fisika Modern',
                 'tahun_pengadaan' => '2016',
                 'fungsi' => ' Mengukur Arus dan Tegangan',
+                'jumlah' => '2'
             ],
 
             [
-                // 'slug' => 'CAU',
+                'slug' => 'CAU',
                 'nama_alat' => 'Catu Daya',
                 'lokasi' => 'Lab Fisika Modern',
                 'tahun_pengadaan' => '2016',
                 'fungsi' => 'Mengalirkan tegangan dengan mengubah tegangan AC ke DC ',
+                'jumlah' => '1'
             ],
             [
-                // 'slug' => 'AUO',
-                'nama_alat' => ' Audio Frequency Generator GW/GRG-450B',
+                'slug' => 'AUO',
+                'nama_alat' => 'Audio Frequency Generator GW/GRG-450B',
                 'lokasi' => 'Lab Elektronika dan Instrumentasi ',
                 'tahun_pengadaan' => '2016',
                 'fungsi' => 'Untuk menghasilkan sinyal pada frekuensi tertentu ',
+                'jumlah' => '1'
             ],
         ];
         DB::table('equipment_categories')->insert($equipmentCategories);
